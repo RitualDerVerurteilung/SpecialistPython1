9093fe1902df6bda12dbdabf928f6c3b53d413eb
RitualDerVerurteilung <110337762+RitualDerVerurteilung@users.noreply.github.com>
2022-08-13 19:59:11 +0300

    Update 09_task_tuple.py
    Для меня эта задачка оказалась довольно сложной (по большей части из-за ошибок, которые постоянно возникали). Даже горжусь, что решил её. Большое спасибо за предоставление таких задач.

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -1,4 +1,29 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+tupl1 = (1, 2, 3, 4)
+tupl2 = (4, 1, 2, 8)
+tupl3 = (-2, 2, 7, 1)
+
+itog = 0
+i = 0
+e = 0
+f = 0
+
+while True:
+    if len(tupl1) == i:
+        break
+    if tupl1[i] == tupl2[e] == tupl3[f]:
+        itog += 1
+        i += 1
+        e = 0
+        f = 0
+    e += 1
+    if len(tupl3) - 1 == f and len(tupl2) == e:
+        i += 1
+        e = 0
+        f = 0
+    if len(tupl2) == e:
+        e = 0
+        f += 1
+print(itog)
